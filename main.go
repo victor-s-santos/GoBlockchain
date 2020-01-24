09393aede8185d9785b3750023710de7082af77d
victor-s-santos <victor.s.silva@unesp.br>
2020-01-24 00:07:32 -0300

    implemented the function which check if the parameters match

--- main.go
+++ main.go
@@ -44,3 +44,16 @@ func generateBlock(oldBlock Block, BPM int) (Block, error) {
 
 	return newBlock, nil
 }
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+	return true
+}
